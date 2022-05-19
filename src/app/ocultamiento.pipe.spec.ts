28fd1eca33f8b7c32a01d94692ef9e5cd9d6a25d
Santiago <santicarfranco8@gmail.com>
2022-05-18 23:35:13 -0500

    Arreglos

--- src/app/ocultamiento.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OcultamientoPipe } from './ocultamiento.pipe';
-
-describe('OcultamientoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new OcultamientoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
